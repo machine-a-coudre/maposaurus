6c723c616760de87de2cb494746cec6d644bd4ab
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-07-19 14:16:24 +0200

    feat: add i18n

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,31 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+const i18n = createI18n({
+  locale: "fr",
+  fallbackLocale: "en",
+  messages: {
+    en: {
+      message: {
+        hello: "hello world",
+      },
+    },
+    fr: {
+      message: {
+        hello: "bonjour",
+      },
+    },
+  },
+});
 
 app.use(createPinia());
+app.use(i18n);
 app.use(router);
 
 app.mount("#app");
